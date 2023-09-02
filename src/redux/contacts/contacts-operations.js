c98d98a97d276ceabfcf1da48b05b7f0e8ba9c3a
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-09-02 13:13:49 +0300

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -13,22 +13,7 @@ export const fetchContacts = createAsyncThunk(
     }
   }
 );
-
-const isDublicate = ({ name, number }, contacts) => {
-    const normalizedName = name.toLowerCase();
-    const normalizedNumber = number.toLowerCase();
-
-    const result = contacts.find(item => {
-      return (
-        normalizedName === item.name.toLowerCase() ||
-        normalizedNumber === item.number.toLowerCase()
-      );
-    });
-
-    return Boolean(result);
-  };
-
-  export const addContact = createAsyncThunk(
+export const addContact = createAsyncThunk(
     'contacts/add',
     async (data, { rejectWithValue }) => {
       try {
@@ -36,17 +21,9 @@ const isDublicate = ({ name, number }, contacts) => {
         return result;
       } catch (error) {
         return rejectWithValue(error);
-      }
-    },
-    {
-      condition: (data, { getState }) => {
-        const { contacts } = getState();
-        if (isDublicate(data, contacts.items)) {
-          alert(`${data.name} or ${data.number} is alredy exist`);
-          return false;
-        }
-      },
+
     }
+}
   );
 
   export const removeContact = createAsyncThunk(
@@ -57,7 +34,8 @@ const isDublicate = ({ name, number }, contacts) => {
         return id;
       } catch (error) {
         return rejectWithValue(error);
-      }
     }
+}
   );
+
 
